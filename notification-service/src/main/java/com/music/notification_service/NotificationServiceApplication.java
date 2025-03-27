85c5b2f84d0c515cd7ca5eae5dfb54953abaec87
hwandefan <hwandefan@gmail.com>
2025-03-27 16:23:50 +0100

    Setup ELK with Logback and Logstash

--- notification-service/src/main/java/com/music/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/music/notification_service/NotificationServiceApplication.java
@@ -2,12 +2,17 @@ package com.music.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(NotificationServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+		logger.info("Notifications service is started");
 	}
 
 }
--- streaming-service/src/main/java/com/music/streaming_service/StreamingServiceApplication.java
+++ streaming-service/src/main/java/com/music/streaming_service/StreamingServiceApplication.java
@@ -2,12 +2,17 @@ package com.music.streaming_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class StreamingServiceApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(StreamingServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(StreamingServiceApplication.class, args);
+		logger.info("Streaming service is started");
 	}
 
 }
